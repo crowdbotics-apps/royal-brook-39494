74fc9e5fb250985d56b950ab7e41e8ddaa9bcde6
Crowdbotics <team@crowdbotics.com>
2023-03-22 06:07:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-brook-39494",
+  url: "https://royal-brook-39494.botics.co",
+  api: "https://royal-brook-39494.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
